9435f2a08c48a2da3ed7c127e7679a148f4a3d02
William Cowell <me@williamcowell.com>
2016-10-03 10:05:50 +0100

    #1 Stateful Dispatcher Service disposes of Reliable Dispatcher.

--- NuGet Libraries/MicroServiceFabric.Dispatcher/StatefulDispatcherService.cs
+++ NuGet Libraries/MicroServiceFabric.Dispatcher/StatefulDispatcherService.cs
@@ -1,17 +1,37 @@
+using System;
 using System.Fabric;
 using System.Threading;
 using System.Threading.Tasks;
+using MicroServiceFabric.CodeContracts;
 using Microsoft.ServiceFabric.Data;
 using Microsoft.ServiceFabric.Services.Runtime;
 
 namespace MicroServiceFabric.Dispatcher
 {
-    public abstract class StatefulDispatcherService<T> : StatefulService
+    public abstract class StatefulDispatcherService<T> : StatefulService, IDisposable
     {
+        private readonly IReliableDispatcher<T> _reliableDispatcher;
+
         protected StatefulDispatcherService(StatefulServiceContext serviceContext,
             IReliableDispatcher<T> reliableDispatcher) : base(serviceContext)
         {
-            
+            _reliableDispatcher = reliableDispatcher;
+            Contract.RequiresNotNull(reliableDispatcher, nameof(reliableDispatcher));
+        }
+
+        void IDisposable.Dispose()
+        {
+            _reliableDispatcher.Dispose();
+        }
+
+        protected override Task RunAsync(CancellationToken cancellationToken)
+        {
+            return RunDispatcherAsync(cancellationToken);
+        }
+
+        internal Task RunDispatcherAsync(CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
         }
 
         protected internal abstract Task OnItemDispatched(ITransaction transaction, T item,
--- Tests/MicroServiceFabric.Dispatcher.Tests/StatefulDispatcherServiceTests.cs
+++ Tests/MicroServiceFabric.Dispatcher.Tests/StatefulDispatcherServiceTests.cs
@@ -1,9 +1,68 @@
-﻿namespace MicroServiceFabric.Dispatcher.Tests
+﻿using System;
+using System.Fabric;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.ServiceFabric.Data;
+using NSubstitute;
+using Xunit;
+
+namespace MicroServiceFabric.Dispatcher.Tests
 {
     public sealed class StatefulDispatcherServiceTests
     {
-        public void ctor_ServiceContextRequired() { }
+        [Fact]
+        public void ctor_ReliableDispatcherRequired()
+        {
+            Assert.Throws<ArgumentNullException>(
+                "reliableDispatcher",
+                () =>
+                    new TestStatefulDispatcherService<object>(
+                        CreateStatefulServiceContext(),
+                        null));
+        }
 
-        public void ctor_ReliableDispatcherRequired() { }
+        [Fact]
+        public void Dispose_DisposesReliableDispatcher()
+        {
+            var reliableDispatcher = Substitute.For<IReliableDispatcher<object>>();
+            IDisposable service = CreateStatefulDispatcherService(reliableDispatcher);
+
+            service.Dispose();
+
+            reliableDispatcher
+                .Received()
+                .Dispose();
+        }
+
+        private static TestStatefulDispatcherService<T> CreateStatefulDispatcherService<T>(IReliableDispatcher<T> reliableDispatcher)
+        {
+            return new TestStatefulDispatcherService<T>(
+                CreateStatefulServiceContext(),
+                reliableDispatcher);
+        }
+
+        private static StatefulServiceContext CreateStatefulServiceContext()
+        {
+            return new StatefulServiceContext(
+                new NodeContext("", new NodeId(0, 1), 0, "Type","localhost"),
+                Substitute.For<ICodePackageActivationContext>(),
+                "ServiceTypeName",
+                new Uri("fabric:/App/ServiceTypeName", UriKind.Absolute),
+                null,
+                Guid.Empty,
+                0L);
+        }
+
+        private sealed class TestStatefulDispatcherService<T> : StatefulDispatcherService<T>
+        {
+            public TestStatefulDispatcherService(StatefulServiceContext serviceContext, IReliableDispatcher<T> reliableDispatcher) : base(serviceContext, reliableDispatcher)
+            {
+            }
+
+            protected override Task OnItemDispatched(ITransaction transaction, T item, CancellationToken cancellationToken)
+            {
+                throw new NotImplementedException();
+            }
+        }
     }
 }
