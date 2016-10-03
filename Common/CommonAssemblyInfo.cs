41762ba569def276815e99d105e80c6b73f7747e
William Cowell <me@williamcowell.com>
2016-10-03 10:37:33 +0100

    #1 Versioning.

---
+++ Common/CommonAssemblyInfo.cs
@@ -0,0 +1,19 @@
+using System.Reflection;
+
+[assembly: AssemblyCompany("William Cowell Consulting Limited")]
+[assembly: AssemblyProduct("Micro Service Fabric")]
+[assembly: AssemblyCopyright("Copyright © 2016 William Cowell Consulting Limited. All rights reserved.")]
+[assembly: AssemblyTrademark("")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
--- NuGet Libraries/MicroServiceFabric.Dispatcher/Properties/AssemblyInfo.cs
+++ NuGet Libraries/MicroServiceFabric.Dispatcher/Properties/AssemblyInfo.cs
@@ -5,13 +5,9 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("MicroServiceFabric.Dispatcher")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("Reliable Dispatcher")]
+[assembly: AssemblyDescription("Provides an [event dispatcher](https://en.wikipedia.org/wiki/Event_loop) (ReliableDispatcher) and an abstract service implementation of an event dispatcher (StatefulDispatcherService). The dispatcher builds upon the Service Fabric [IReliableQueue](https://msdn.microsoft.com/en-us/library/azure/dn971527.aspx?f=255&MSPPError=-2147217396).")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("MicroServiceFabric.Dispatcher")]
-[assembly: AssemblyCopyright("Copyright ©  2016")]
-[assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
@@ -21,17 +17,4 @@ using System.Runtime.InteropServices;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("c330b920-c5e9-4d9a-8e22-0a082bb85410")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
 [assembly: InternalsVisibleTo("MicroServiceFabric.Dispatcher.Tests")]
