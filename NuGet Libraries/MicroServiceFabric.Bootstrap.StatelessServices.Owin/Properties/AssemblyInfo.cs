19ced1f7a68e1626dcd2583fd8c7ae6b9971472a
William Cowell <me@williamcowell.com>
2016-10-04 16:50:44 +0100

    #5 Created project for bootstrapping OWIN stateless services

---
+++ NuGet Libraries/MicroServiceFabric.Bootstrap.StatelessServices.Owin/Properties/AssemblyInfo.cs
@@ -0,0 +1,18 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("OWIN Stateless Service Bootstrap")]
+[assembly: AssemblyDescription("Bootstrap for Service Fabric OWIN Stateless Services")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("d2f0e77f-1575-4096-8916-8b3a0011d8ce")]
