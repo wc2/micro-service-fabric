03b97b5a4c6f644bb52eb6d1cb18810c97c39d1c
William Cowell <me@williamcowell.com>
2016-10-04 09:45:48 +0100

    #1 Add some projects that were missing from the CounterApp sample.

---
+++ Samples/CounterApp_ReliableDispatcher/CounterService.Api/IQueryCounter.cs
@@ -0,0 +1,10 @@
+﻿using System.Threading.Tasks;
+using Microsoft.ServiceFabric.Services.Remoting;
+
+namespace CounterService.Api
+{
+    public interface IQueryCounter : IService
+    {
+        Task<int> GetCounterAsync();
+    }
+}
---
+++ Samples/CounterApp_ReliableDispatcher/CounterWebApi/Controllers/CounterController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Web.Http;
+using CounterService.Api;
+using Microsoft.ServiceFabric.Services.Client;
+using Microsoft.ServiceFabric.Services.Remoting.Client;
+
+namespace CounterWebApi.Controllers
+{
+    public class CounterController : ApiController
+    {
+        private readonly IQueryCounter _queryCounter;
+        private readonly IIncrementCounter _incrementCounter;
+
+        public CounterController()
+        {
+            var serviceUri = new Uri("fabric:/CounterApp/CounterService");
+            var partitionKey = new ServicePartitionKey(0);
+
+            _queryCounter = ServiceProxy.Create<IQueryCounter>(serviceUri, partitionKey);
+            _incrementCounter = ServiceProxy.Create<IIncrementCounter>(serviceUri, partitionKey);
+        }
+
+        public async Task<IHttpActionResult> Get()
+        {
+            var counter = await _queryCounter.GetCounterAsync().ConfigureAwait(false);
+            return Ok(counter);
+        }
+
+        public async Task<IHttpActionResult> Post([FromBody] int incrementCounterBy)
+        {
+            await _incrementCounter.IncrementCounterAsync(incrementCounterBy).ConfigureAwait(false);
+            return Ok();
+        }
+    }
+}
