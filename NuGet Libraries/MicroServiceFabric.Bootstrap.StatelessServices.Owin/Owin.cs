0b3cdb7cd67e88cfa4b591b7e218f2f93ea1c9c5
William Cowell <me@williamcowell.com>
2016-10-05 09:37:45 +0100

    #5 Fixed issue with multiple constructors.

--- NuGet Libraries/MicroServiceFabric.Bootstrap.StatelessServices.Owin/Owin.cs
+++ NuGet Libraries/MicroServiceFabric.Bootstrap.StatelessServices.Owin/Owin.cs
@@ -11,23 +11,16 @@ namespace MicroServiceFabric.Bootstrap.StatelessServices.Owin
     internal sealed class Owin : StatelessService
     {
         private readonly Action<IAppBuilder> _appBuilder;
-        private readonly string _endpointName;
         private readonly IServiceEventSource _serviceEventSource;
 
         public Owin(StatelessServiceContext serviceContext, IServiceEventSource serviceEventSource,
-            Action<IAppBuilder> appBuilder) : this(serviceContext, serviceEventSource, appBuilder, "ServiceEndpoint")
-        {
-        }
-
-        public Owin(StatelessServiceContext serviceContext, IServiceEventSource serviceEventSource,
-            Action<IAppBuilder> appBuilder, string endpointName) : base(serviceContext)
+            Action<IAppBuilder> appBuilder) : base(serviceContext)
         {
             Contract.RequiresNotNull(serviceEventSource, nameof(serviceEventSource));
             Contract.RequiresNotNull(appBuilder, nameof(appBuilder));
 
             _serviceEventSource = serviceEventSource;
             _appBuilder = appBuilder;
-            _endpointName = endpointName;
         }
 
         protected override IEnumerable<ServiceInstanceListener> CreateServiceInstanceListeners()
@@ -36,7 +29,7 @@ namespace MicroServiceFabric.Bootstrap.StatelessServices.Owin
             {
                 new ServiceInstanceListener(
                     serviceContext =>
-                        new OwinCommunicationListener(_appBuilder, serviceContext, _serviceEventSource, _endpointName))
+                        new OwinCommunicationListener(_appBuilder, serviceContext, _serviceEventSource, "ServiceEndpoint"))
             };
         }
     }
