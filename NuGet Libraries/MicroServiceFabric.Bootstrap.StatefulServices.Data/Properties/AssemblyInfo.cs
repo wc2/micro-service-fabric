ce2c27a4ea7fcbf3ee242dfc3960bf4b6d46bc46
William Cowell <recruit-me@williamcowell.com>
2017-01-24 12:59:06 +0000

    Creation of NuGet package.

--- NuGet Libraries/MicroServiceFabric.Bootstrap.StatefulServices.Data/Properties/AssemblyInfo.cs
+++ NuGet Libraries/MicroServiceFabric.Bootstrap.StatefulServices.Data/Properties/AssemblyInfo.cs
@@ -1,17 +1,12 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("MicroServiceFabric.Bootrap.StatefulServices.Data")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Package to facilitate bootstrapping Reliable Services that require Reliable Collections upon construction.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("MicroServiceFabric.Bootrap.StatefulServices.Data")]
-[assembly: AssemblyCopyright("Copyright ©  2017")]
-[assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
@@ -21,16 +16,3 @@ using System.Runtime.InteropServices;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("a8031622-50da-446a-b68b-d8a36ead5b5c")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
